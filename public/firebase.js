2655aa0e8409cead70041d215e5db4954c6c3c01
Omar Colmenares <omarnazaret@gmail.com>
2020-07-01 16:18:04 -0400

    update .gitignore

---
+++ public/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAT2It9YfkGx2r5d2PutrsaYMD06nhGDm0",
+    authDomain: "fir-crud-45c60.firebaseapp.com",
+    databaseURL: "https://fir-crud-45c60.firebaseio.com",
+    projectId: "fir-crud-45c60",
+    storageBucket: "fir-crud-45c60.appspot.com",
+    messagingSenderId: "767398662571",
+    appId: "1:767398662571:web:aa5dff17c5adeb720882ee"
+};
+// Initialize Firebase
